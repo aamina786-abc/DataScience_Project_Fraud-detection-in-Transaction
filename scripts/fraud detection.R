128b05c08ef1d246b88021fd08ac2adfff203933
aamina786-abc <piyarjiaamina123@gmail.com>
2026-01-09 02:01:10 +0530

    Update fraud detection.R

--- scripts/fraud detection.R
+++ scripts/fraud detection.R
@@ -71,50 +71,37 @@ train_balanced <- ROSE(Class ~ ., data = train_clean, seed = 1)$data
 # Check the distribution
 table(train_balanced$Class)
 
-#7 Logistic Regression Model (optional)
-log_model <- glm(Class ~ ., data = train_balanced, family = binomial)
-summary(log_model)
-
-str(train)   # see types in training data
-str(test)    # see types in test data
-
-test$Time <- as.numeric(test$Time)
-test$Amount <- as.numeric(test$Amount)
-                                
-test$Class <- as.factor(test$Class)  # if needed
-
-#7.a Predictions
-log_pred_prob <- predict(log_model, test, type = "response")
-log_pred <- ifelse(log_pred_prob > 0.5, 1, 0)
-
-install.packages("caret")
-library(caret)
-#7.b Evaluation
-confusionMatrix(as.factor(log_pred), as.factor(test$Class))
-
 #8 Random Forest Model
-install.packages("randomForest")
 library(randomForest)
 
+# Train classification Random Forest
 rf_model <- randomForest(
-  Class ~ ., 
-  data = train_balanced, 
+  Class ~ .,
+  data = train_balanced,
   ntree = 100,
   importance = TRUE
 )
-                                
-# Predictions
-rf_pred <- predict(rf_model, test)
-confusionMatrix(rf_pred, as.factor(test$Class))
 
-#9 ROC Curve & AUC
-roc_obj <- roc(test$Class, as.numeric(log_pred_prob))
-plot(roc_obj, col = "blue", main = "ROC Curve")
-auc(roc_obj)
+# Verify model type
+rf_model$type   # MUST be "classification"
 
-#10 Identify Suspicious Transactions
-suspicious_txns <- test[log_pred == 1, ]
-head(suspicious_txns)
+library(caret)
+# Class prediction
+rf_pred <- predict(rf_model, newdata = test)
+confusionMatrix(rf_pred, test$Class)
+
+# Probability prediction (for ROC)
+rf_prob <- predict(rf_model, newdata = test, type = "prob")[, 2]
+
+library(pROC)
+# ROC Curve
+roc_rf <- roc(test$Class, rf_prob)
+plot(roc_rf, col = "red", main = "Random Forest ROC Curve")
+
+# AUC value
+auc(roc_rf)
+
+
 
 
 
